237c0f04a7f937f2a4fc25ca94f5c6e283b6e58a
withallthesequestions <uytpic434516@gmail.com>
2025-10-18 21:30:40 -0400

    testing urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tremendous-telescope-six.vercel.app',
+	base: 'https://tremendous-telescope-six.vercel.app',
 	integrations: [mdx()],
 });
