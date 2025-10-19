01c51250f1bb15665904a9a9226b1c34ee8fc5d0
withallthesequestions <uytpic434516@gmail.com>
2025-10-18 21:27:38 -0400

    nobase

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tremendous-telescope-six.vercel.app',
-	base: 'https://very-tremendous-telescope-six.vercel.app',
 	integrations: [mdx()],
 });
