63a7d9e59f81995e9c84ccced196916a0fd58893
withallthesequestions <uytpic434516@gmail.com>
2025-10-18 21:56:52 -0400

    search function

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tremendous-telescope-six.vercel.app',
-	integrations: [mdx()],
+	integrations: [mdx(), pagefind()],
 });
