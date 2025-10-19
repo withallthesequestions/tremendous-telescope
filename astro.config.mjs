5c0968c200459f536d3f6aec04bde4c05ae570b3
withallthesequestions <uytpic434516@gmail.com>
2025-10-18 20:56:11 -0400

    updated site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	// site: 'https://withallthesequestions.github.io',
+	site: 'https://tremendous-telescope-six.vercel.app/',
 	/* base: '/tremendous-telescope', */
 	integrations: [mdx()],
 });
