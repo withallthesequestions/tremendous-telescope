4b6f38846f327df65e7532052b91357d725e17cc
withallthesequestions <uytpic434516@gmail.com>
2025-10-18 21:22:08 -0400

    is the slash the problem?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tremendous-telescope-six.vercel.app/',
+	site: 'https://tremendous-telescope-six.vercel.app',
 	base: 'https://tremendous-telescope-six.vercel.app/',
 	integrations: [mdx()],
 });
